bea215d2e0a0c10a9f501e15fca0c77e32c5a6ea
gaara <nikel_black@hotmail.com>
2018-12-20 18:33:55 -0600

    cambios final del dia

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const remote = require('electron').remote
-
 const main = remote.require('./main.js')
 
 let button = document.createElement('button')
 
-button.textContent = "open new window"
+button.textContent = "Abrete pofavo"
 document.body.appendChild(button)
 button .addEventListener('click', () =>{
     main.openWindow()
+
 })
--- main.js
+++ main.js
@@ -7,7 +7,9 @@ const url = require('url')
 let win
 
 function createWindow() {
-    win = new BrowserWindow({width:800, height:600})
+    win = new BrowserWindow({
+        width:800, height:600
+    })
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol:'file',
